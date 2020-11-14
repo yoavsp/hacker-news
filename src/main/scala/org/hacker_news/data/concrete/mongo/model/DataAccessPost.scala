cfe34c5a6b3015a7a6743d6bc9790cf9ab412dee
yoavsadeh <yoav.sadeh@saga.org>
2020-11-14 13:22:55 +0200

    removed redundant score field

--- src/main/scala/org/hacker_news/data/concrete/mongo/model/DataAccessPost.scala
+++ src/main/scala/org/hacker_news/data/concrete/mongo/model/DataAccessPost.scala
@@ -3,8 +3,8 @@ package org.hacker_news.data.concrete.mongo.model
 import org.mongodb.scala.bson.ObjectId
 
 object DataAccessPost{
-  def apply(title: String, text: String, score: Int, createdAt: Long, votes: Int): DataAccessPost =
-    DataAccessPost(new ObjectId(), title, text, score, createdAt, votes)
+  def apply(title: String, text: String, createdAt: Long, votes: Int): DataAccessPost =
+    DataAccessPost(new ObjectId(), title, text, createdAt, votes)
 }
 
-case class DataAccessPost(_id: ObjectId, title: String, text: String, score: Int, createdAt: Long, votes: Int)
+case class DataAccessPost(_id: ObjectId, title: String, text: String, createdAt: Long, votes: Int)
